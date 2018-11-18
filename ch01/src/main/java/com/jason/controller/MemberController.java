57648a374c02b1e16e9b68f9db910ef1eb88e181
kohyusik <newtype26@naver.com>
2018-11-19 02:13:20 +0900

    logback 설정 추가

--- ch01/src/main/java/com/jason/controller/MemberController.java
+++ ch01/src/main/java/com/jason/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.jason.controller;
 
 import com.jason.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @description :
  */
 
+@Slf4j
 @Controller
 public class MemberController {
 
@@ -24,13 +26,15 @@ public class MemberController {
     @RequestMapping("/time")
     @ResponseBody
     public String time() {
+        log.debug("MemberController.time()...");
         return memberService.getTime();
     }
 
     @RequestMapping("/time2")
     @ResponseBody
     public String time2() {
+        log.debug("MemberController.time2()...");
         return memberService.getTime2();
     }
-    
+
 }
