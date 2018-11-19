a44f528ef559c6f5c1493c7f85dd2381feb0c632
kohyusik <newtype26@naver.com>
2018-11-20 00:59:17 +0900

    move web packge

--- ch01/src/test/java/com/jason/web/DataSourceTest.java
+++ ch01/src/test/java/com/jason/web/DataSourceTest.java
@@ -1,6 +1,6 @@
 package com.jason.web;
 
-import com.jason.mapper.MemberMapper;
+import com.jason.web.mapper.MemberMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
