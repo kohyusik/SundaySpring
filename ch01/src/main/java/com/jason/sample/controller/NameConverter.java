0c5b9866ff06230cb627c260580b3675c191dcdc
kohyusik <newtype26@naver.com>
2018-11-25 15:13:05 +0900

    file temp name 생성 : Functional interface

---
+++ ch01/src/main/java/com/jason/sample/controller/NameConverter.java
@@ -0,0 +1,12 @@
+package com.jason.sample.controller;
+
+/**
+ * @author : yusik
+ * @date : 25/11/2018
+ */
+
+@FunctionalInterface
+public interface NameConverter {
+
+    String convert(String name);
+}
